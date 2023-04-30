dd4ecef1a9b7862c3ae91508b1c6a914783a2d00
Meeth <mjdavda02@gmail.com>
2023-04-30 12:42:49 +0530

    smart contract redeploy

--- block_backend/scripts/deploy.js
+++ block_backend/scripts/deploy.js
@@ -7,6 +7,29 @@ async function main() {
   await contract.deployed();
 
   console.log("Contract address:", contract.address);
+  //contract address - 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
+  saveFrontendFiles(contract, "NFT");
+}
+
+function saveFrontendFiles(contract, name) {
+  const fs = require("fs");
+  const contractsDir = __dirname + "/../../frontend/contractsData";
+
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  fs.writeFileSync(
+    contractsDir + `/${name}-address.json`,
+    JSON.stringify({ address: contract.address }, undefined, 2)
+  );
+
+  const contractArtifact = artifacts.readArtifactSync(name);
+
+  fs.writeFileSync(
+    contractsDir + `/${name}.json`,
+    JSON.stringify(contractArtifact, null, 2)
+  );
 }
 
 main().catch((error) => {
